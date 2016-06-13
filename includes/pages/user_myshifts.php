24ec8907107a4be3423ce52da81f39c8bde19b29
kamishetty sreeja <sreeja25kamishetty@gmail.com>
2016-06-13 16:47:42 +0530

    changed the functions indentation

--- includes/pages/user_myshifts.php
+++ includes/pages/user_myshifts.php
@@ -1,38 +1,28 @@
 <?php
-function myshifts_title()
- {
+function myshifts_title() {
   return _("My shifts");
- }
+}
 // Zeigt die Schichten an, die ein Benutzer belegt
-function user_myshifts()
- {
+function user_myshifts() {
   global $LETZTES_AUSTRAGEN;
   global $user, $privileges;
   $msg = "";
-  if (isset($_REQUEST['id']) && in_array("user_shifts_admin", $privileges) && preg_match("/^[0-9]{1,}$/", $_REQUEST['id']) && sql_num_query("SELECT * FROM `User` WHERE `UID`='" . sql_escape($_REQUEST['id']) . "'") > 0)
-   {
+  if (isset($_REQUEST['id']) && in_array("user_shifts_admin", $privileges) && preg_match("/^[0-9]{1,}$/", $_REQUEST['id']) && sql_num_query("SELECT * FROM `User` WHERE `UID`='" . sql_escape($_REQUEST['id']) . "'") > 0) {
     $id = $_REQUEST['id'];
-   }
-  else
-   {
+  } else
     $id = $user['UID'];
-   }
   list($shifts_user) = sql_select("SELECT * FROM `User` WHERE `UID`='" . sql_escape($id) . "' LIMIT 1");
-  if (isset($_REQUEST['reset']))
-   {
-    if ($_REQUEST['reset'] == "ack")
-     {
+  if (isset($_REQUEST['reset'])) {
+    if ($_REQUEST['reset'] == "ack") {
       User_reset_api_key($user);
       success(_("Key changed."));
       redirect(page_link_to('users') . '&action=view&user_id=' . $shifts_user['UID']);
-     }
+    }
     return page_with_title(_("Reset API key"), array(
       error(_("If you reset the key, the url to your iCal- and JSON-export and your atom feed changes! You have to update it in every application using one of these exports."), true),
       button(page_link_to('user_myshifts') . '&reset=ack', _("Continue"), 'btn-danger')
     ));
-   }
-  elseif (isset($_REQUEST['edit']) && preg_match("/^[0-9]*$/", $_REQUEST['edit']))
-   {
+  } elseif (isset($_REQUEST['edit']) && preg_match("/^[0-9]*$/", $_REQUEST['edit'])) {
     $id    = $_REQUEST['edit'];
     $shift = sql_select("SELECT
         `ShiftEntry`.`freeloaded`,
@@ -50,28 +40,23 @@ function user_myshifts()
         JOIN `Room` ON (`Shifts`.`RID` = `Room`.`RID`)
         WHERE `ShiftEntry`.`id`='" . sql_escape($id) . "'
         AND `UID`='" . sql_escape($shifts_user['UID']) . "' LIMIT 1");
-    if (count($shift) > 0)
-     {
+    if (count($shift) > 0) {
       $shift            = $shift[0];
       $freeloaded       = $shift['freeloaded'];
       $freeload_comment = $shift['freeload_comment'];
-      if (isset($_REQUEST['submit']))
-       {
+      if (isset($_REQUEST['submit'])) {
         $ok = true;
-        if (in_array("user_shifts_admin", $privileges))
-         {
+        if (in_array("user_shifts_admin", $privileges)) {
           $freeloaded       = isset($_REQUEST['freeloaded']);
           $freeload_comment = strip_request_item_nl('freeload_comment');
-          if ($freeloaded && $freeload_comment == '')
-           {
+          if ($freeloaded && $freeload_comment == '') {
             $ok = false;
             error(_("Please enter a freeload comment!"));
-           }
-         }
+          }
+        }
         $comment     = strip_request_item_nl('comment');
         $user_source = User($shift['UID']);
-        if ($ok)
-         {
+        if ($ok) {
           $result = ShiftEntry_update(array(
             'id' => $id,
             'Comment' => $comment,
@@ -83,26 +68,21 @@ function user_myshifts()
           engelsystem_log("Updated " . User_Nick_render($user_source) . "'s shift " . $shift['name'] . " from " . date("Y-m-d", $shift['start']) . " " . date("H:i", $shift['start_time']) . "to " . date("Y-m-d", $shift['end']) . " " . date("H:i", $shift[end_time]) . " with comment " . $comment . ". Freeloaded: " . ($freeloaded ? "YES Comment: " . $freeload_comment : "NO"));
           success(_("Shift saved."));
           redirect(page_link_to('users') . '&action=view&user_id=' . $shifts_user['UID']);
-         }
-       }
+        }
+      }
       return ShiftEntry_edit_view(User_Nick_render($shifts_user), date("Y-m-d", $shift['start']) . " " . date("H:i", $shift['start_time']) . ', ' . shift_length($shift), $shift['Name'], $shift['name'], $shift['angel_type'], $shift['Comment'], $shift['freeloaded'], $shift['freeload_comment'], in_array("user_shifts_admin", $privileges));
-     }
-    else
+    } else
       redirect(page_link_to('user_myshifts'));
-   }
-  elseif (isset($_REQUEST['cancel']) && preg_match("/^[0-9]*$/", $_REQUEST['cancel']))
-   {
+  } elseif (isset($_REQUEST['cancel']) && preg_match("/^[0-9]*$/", $_REQUEST['cancel'])) {
     $id    = $_REQUEST['cancel'];
     $shift = sql_select("
         SELECT *
         FROM `Shifts` 
         INNER JOIN `ShiftEntry` USING (`SID`) 
         WHERE `ShiftEntry`.`id`='" . sql_escape($id) . "' AND `UID`='" . sql_escape($shifts_user['UID']) . "'");
-    if (count($shift) > 0)
-     {
+    if (count($shift) > 0) {
       $shift = $shift[0];
-      if (($shift['start'] > time() + $LETZTES_AUSTRAGEN * 3600) || in_array('user_shifts_admin', $privileges))
-       {
+      if (($shift['start'] > time() + $LETZTES_AUSTRAGEN * 3600) || in_array('user_shifts_admin', $privileges)) {
         $result = ShiftEntry_delete($id);
         if ($result === false)
           engelsystem_error('Unable to delete shift entry.');
@@ -111,13 +91,11 @@ function user_myshifts()
         $shifttype = ShiftType($shift['shifttype_id']);
         engelsystem_log("Deleted own shift: " . $shifttype['name'] . " at " . $room['Name'] . " from " . date("Y-m-d", $shift['start']) . " " . date("H:i", $shift['start_time']) . " to " . date("Y-m-d", $shift['end']) . " " . date("H:i", $shift['end_time']) . " as " . $angeltype['name']);
         success(_("Shift canceled."));
-       }
-      else
+      } else
         error(_("It's too late to sign yourself off the shift. If neccessary, ask the dispatcher to do so."));
-     }
-    else
+    } else
       redirect(user_link($shifts_user));
-   }
+  }
   redirect(page_link_to('users') . '&action=view&user_id=' . $shifts_user['UID']);
- }
+}
 ?>
