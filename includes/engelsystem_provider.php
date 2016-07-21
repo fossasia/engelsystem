533a72a35b8705d15b27ee7df9bd5be1eb7fb2f5
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-07-21 09:47:37 +0530

    Implement MVC Model

--- includes/engelsystem_provider.php
+++ includes/engelsystem_provider.php
@@ -11,6 +11,7 @@ require_once realpath(__DIR__ . '/../includes/sys_page.php');
 require_once realpath(__DIR__ . '/../includes/sys_template.php');
 
 require_once realpath(__DIR__ . '/../includes/model/AngelType_model.php');
+require_once realpath(__DIR__ . '/../includes/model/guest_stats_model.php');
 require_once realpath(__DIR__ . '/../includes/model/LogEntries_model.php');
 require_once realpath(__DIR__ . '/../includes/model/Message_model.php');
 require_once realpath(__DIR__ . '/../includes/model/NeededAngelTypes_model.php');
@@ -18,6 +19,7 @@ require_once realpath(__DIR__ . '/../includes/model/Room_model.php');
 require_once realpath(__DIR__ . '/../includes/model/ShiftEntry_model.php');
 require_once realpath(__DIR__ . '/../includes/model/Shifts_model.php');
 require_once realpath(__DIR__ . '/../includes/model/ShiftTypes_model.php');
+require_once realpath(__DIR__ . '/../includes/model/user_atom_model.php');
 require_once realpath(__DIR__ . '/../includes/model/UserAngelTypes_model.php');
 require_once realpath(__DIR__ . '/../includes/model/UserDriverLicenses_model.php');
 require_once realpath(__DIR__ . '/../includes/model/UserGroups_model.php');
