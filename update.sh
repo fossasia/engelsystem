3597801676f08061f4a630dc4218b525975e6194
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-08-07 22:31:54 +0530

    Created Update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Authors:
+#   Dishant Khanna <dishant.khanna1807@gmail.com> [FOSSASIA]
+
+# Description:
+#   An update bash script for engelsystem
+
+# Add upstream 
+git remote add upstream http://www.github.com/fossasia/engelsystem.git
+
+# Pull the updates
+git pull upstream development
