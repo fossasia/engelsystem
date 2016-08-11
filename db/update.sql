92204d78a3b493559e2ac8114b061643b2f674d4
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-08-11 12:55:36 +0530

    Update update.sql

--- db/update.sql
+++ db/update.sql
@@ -66,6 +66,7 @@ CREATE TABLE IF NOT EXISTS `Settings` (
   `event_start_date` int(11) DEFAULT NULL,
   `event_end_date` int(11) DEFAULT NULL,
   `teardown_end_date` int(11) DEFAULT NULL,
+  `autoupdate` tinyint(1) NOT NULL DEFAULT '0',
   `event_welcome_msg` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
 
